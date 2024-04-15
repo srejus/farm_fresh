829aed1a227d323b6b59138604558d652f3771e0
srejus <srejusjayakumar@gmail.com>
2024-04-15 15:41:18 +0530

    final changes

--- admin_user/urls.py
+++ admin_user/urls.py
@@ -4,6 +4,14 @@ from .views import *
 urlpatterns = [
     path('',AdminHomeView.as_view()),
     path('users',AdminUserView.as_view()),
+
+    path('items',AdminItemsView.as_view()),
+    path('items/delete/<int:id>',AdminItemsView.as_view()),
+    path('items/add',AdminAddItemView.as_view()),
+
+    path('orders',AdminOrdersView.as_view()),
+    path('orders/<int:id>',AdminOrdersView.as_view()),
+
     path('feeds',AdminFeedView.as_view()),
     path('feeds/<int:id>',AdminFeedView.as_view()),
     path('notifications',AdminNotiView.as_view()),
--- admin_user/views.py
+++ admin_user/views.py
@@ -8,7 +8,7 @@ from django.utils.decorators import method_decorator
 from accounts.models import *
 from feeds.models import Feed
 from home.models import *
-
+from store.models import *
 
 # Create your views here.
 @method_decorator(login_required,name='dispatch')
@@ -25,6 +25,43 @@ class AdminUserView(View):
     def get(self,request):
         users = Account.objects.all()
         return render(request,'Admin_users.html',{'users':users})
+
+@method_decorator(login_required,name='dispatch')
+class AdminItemsView(View):
+    def get(self,request,id=None):
+        if id:
+            Product.objects.get(id=id).delete()
+            return redirect("/admin-user/items")
+
+        items = Product.objects.all().order_by('-id')
+        return render(request,'Admin_items.html',{'items':items})
+    
+
+@method_decorator(login_required,name='dispatch')
+class AdminAddItemView(View):
+    def get(self,request):
+        return render(request,'Admin_add_item.html')
+    
+    def post(self,request):
+        title = request.POST.get("title")
+        price = request.POST.get("price")
+        product_image = request.FILES.get("product_image")
+        type_ = request.POST.get("type")
+
+        Product.objects.create(title=title,price=price,product_type=type_,product_img=product_image)
+        return redirect("/admin-user/items")
+    
+
+@method_decorator(login_required,name='dispatch')
+class AdminOrdersView(View):
+    def get(self,request,id=None):
+        if id:
+            order = Order.objects.get(id=id)
+            order_items = OrderItems.objects.filter(order=order)
+            return render(request,'Admin_view_order.html',{'order':order,"items":order_items})
+        
+        orders = Order.objects.all().order_by('-id')
+        return render(request,'Admin_orders.html',{'orders':orders})
 
 @method_decorator(login_required,name='dispatch')
 class AdminNotiView(View):
